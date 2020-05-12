c6f6d822ceffb889661612c9f65f6c6d436c99c2
christian.dennig@outlook.com <christian.dennig@outlook.com>
2020-05-12 13:51:59 +0200

    Fix helm / k8s config

--- day7/terraform/kubernetes/main.tf
+++ day7/terraform/kubernetes/main.tf
@@ -66,7 +66,7 @@ resource "azurerm_public_ip" "ingress_ip" {
 
 provider "helm" {
   kubernetes {
-    host                   = "${azurerm_kubernetes_cluster.k8s.fqdn}"
+    host                   = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
     client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)}"
     client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)}"
     cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)}"
