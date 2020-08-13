1453742daa6928f65959a370f767c3c9998dbbea
Andreas Mock <andreas.mock@live.com>
2020-08-13 14:21:00 +0200

    added scmtopic subscribtions to SB

--- day8/terraform/messaging/main.tf
+++ day8/terraform/messaging/main.tf
@@ -23,6 +23,31 @@ resource "azurerm_servicebus_queue" "queue_thumbnails" {
   namespace_name      = azurerm_servicebus_namespace.sbn.name
 }
 
+# Contacts Topic
+resource "azurerm_servicebus_topic" "contacts" {
+  name                = "scmtopic"
+  resource_group_name = var.resource_group_name
+  namespace_name      = azurerm_servicebus_namespace.sbn.name
+}
+
+resource "azurerm_servicebus_subscription" "contacts_search" {
+  name                = "scmsearch"
+  resource_group_name = var.resource_group_name
+  namespace_name      = azurerm_servicebus_namespace.sbn.name
+  topic_name          = azurerm_servicebus_topic.contacts.name
+  max_delivery_count  = 10
+  requires_session    = true
+}
+
+resource "azurerm_servicebus_subscription" "contacts_visitreport" {
+  name                = "scmvisitreports"
+  resource_group_name = var.resource_group_name
+  namespace_name      = azurerm_servicebus_namespace.sbn.name
+  topic_name          = azurerm_servicebus_topic.contacts.name
+  max_delivery_count  = 10
+  requires_session    = false
+}
+
 output "servicebus_connection_string" {
     value       = azurerm_servicebus_namespace.sbn.default_primary_connection_string
     description = "ServiceBus connection string with RootManagedSharedAccessKey" 
