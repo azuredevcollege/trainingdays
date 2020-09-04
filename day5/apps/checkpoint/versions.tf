d5ad4591a1a7d9978c1587f465ea98ea95095049
Clemens Wältken <clemens.waltken@microsoft.com>
2020-09-04 14:34:21 +0200

    Upgrade to terraform >= 0.13.2

--- day5/apps/checkpoint/versions.tf
+++ day5/apps/checkpoint/versions.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     azuread = {
-
+      source = "hashicorp/azuread"
     }
     azuredevops = {
-
+      source = "terraform-providers/azuredevops"
     }
     azurerm = {
-
+      source = "hashicorp/azurerm"
     }
     null = {
-
+      source = "hashicorp/null"
     }
     random = {
-
+      source = "hashicorp/random"
     }
 
   }
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
 
 }
