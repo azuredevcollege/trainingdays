fc5c19c455b7255be6b792b7f6d1e4de1e223c96
Clemens Wältken <clemens.waltken@microsoft.com>
2022-02-11 10:20:51 +0100

    Add script to enable workflows

---
+++ day5/scripts/enable-workflows-for.sh
@@ -0,0 +1,33 @@
+if [ $# -eq 0 ]
+  then
+    echo "Please provide your GitHub username as argument"
+    exit
+fi
+REMOTE_ORIGIN=$(git remote get-url origin)
+echo "Using GitHub username: $1";
+echo "Asuming GitHub user repo to be: $1/trainingdays"
+echo "Asuming origin to point to: azuredevcollege/trainingdays"
+if [ "$REMOTE_ORIGIN" != "git@github.com:azuredevcollege/trainingdays.git" ]
+  then
+    echo "Remote origin not pointing to trainer repo!"
+    echo "Please run: git remote set-url origin git@github.com:azuredevcollege/trainingdays.git"
+fi
+echo "Reseting master to trainer repo"
+git fetch origin
+git reset origin/master --hard
+for i in .github/workflows/day4-scm-*; do
+    sed -i "s/'azuredevcollege\/trainingdays'/'$1\/trainingdays'/g" $i
+done
+echo "Staging changes..."
+git add .github/workflows/day4-scm-*
+echo "Commiting changes..."
+git commit -m "Enable workflow files for $1/trainingdays"
+if  git remote get-url script > /dev/null 2> /dev/null; then
+  echo "Remote already present."
+else
+  echo "Adding remote 'script'..."
+  git remote add script git@github.com:$1/trainingdays.git
+fi
+echo "Pushing changes..."
+git push script master -f
+echo "Done! Now Add the 'AZURE_CREDENTIALS' and 'SQL_PASSWORD' secrets to you GitHub pipeline."
