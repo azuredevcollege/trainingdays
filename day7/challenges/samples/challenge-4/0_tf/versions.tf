e833e063f3eeed12036930f5888d48fc996fde7d
Clemens Wältken <clemens.waltken@microsoft.com>
2022-03-08 15:45:25 +0100

    Update to terraform v1.1.7

--- day7/challenges/samples/challenge-4/0_tf/versions.tf
+++ day7/challenges/samples/challenge-4/0_tf/versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.1.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.98.0"
+    }
+  }
 }
