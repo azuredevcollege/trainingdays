89b773826f28330fcdd64faa6408b8bb113c1a4a
Andreas Mock <andreas.mock@live.com>
2022-04-29 08:37:15 +0200

    d5 no deprecated packages

--- day5/apps/dotnetcore/Scm.Search/Adc.Scm.Search.Api/Services/ContactSearchService.cs
+++ day5/apps/dotnetcore/Scm.Search/Adc.Scm.Search.Api/Services/ContactSearchService.cs
@@ -1,6 +1,6 @@
 ﻿using Adc.Scm.Search.Api.Models;
-using Microsoft.Azure.Search;
-using Microsoft.Azure.Search.Models;
+using Azure;
+using Azure.Search.Documents;
 using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
@@ -18,30 +18,32 @@ namespace Adc.Scm.Search.Api.Services
 
         public async Task<object> Search(Guid userId, string phrase)
         {
-            var client = new SearchServiceClient(_options.ServiceName, new SearchCredentials(_options.AdminApiKey));
-            var indexClient = client.Indexes.GetClient(_options.IndexName);
-            var result = await indexClient.Documents.SearchAsync($"(userid:{userId.ToString()}) AND ({phrase})", 
-                new SearchParameters() 
-                {
-                    QueryType = QueryType.Full
-                });
+            AzureKeyCredential credential = new AzureKeyCredential(_options.AdminApiKey);
+            SearchClient client = new SearchClient(new Uri($"https://{_options.ServiceName}.search.windows.net"), _options.IndexName, credential);
 
-            return result.Results;
+            var result = await client.SearchAsync<dynamic>($"(userid:{userId}) AND ({phrase})", new SearchOptions
+            {
+                QueryType = Azure.Search.Documents.Models.SearchQueryType.Full
+            });
+
+            return result.Value.GetResults();
         }
 
         public async Task<object> Search(Guid userId, ContactSearch search)
         {
-            var client = new SearchServiceClient(_options.ServiceName, new SearchCredentials(_options.AdminApiKey));
-            var indexClient = client.Indexes.GetClient(_options.IndexName);
-            var result = await indexClient.Documents.SearchAsync($"(userid:{userId.ToString()}) AND ({search.Phrase})",
-                new SearchParameters()
-                {
-                    QueryType = QueryType.Full,
-                    Select = search.SelectFields,
-                    SearchFields = search.SearchFields
-                });
+            AzureKeyCredential credential = new AzureKeyCredential(_options.AdminApiKey);
+            SearchClient client = new SearchClient(new Uri($"https://{_options.ServiceName}.search.windows.net"), _options.IndexName, credential);
 
-            return result.Results;
+            var so = new SearchOptions
+            {
+                QueryType = Azure.Search.Documents.Models.SearchQueryType.Full,
+            };
+            search.SelectFields.ForEach(selField => so.Select.Add(selField));
+            search.SearchFields.ForEach(searchField => so.SearchFields.Add(searchField));
+
+            var result = await client.SearchAsync<dynamic>($"(userid:{userId}) AND ({search.Phrase})", so);
+
+            return result.Value.GetResults();
         }
     }
 }
